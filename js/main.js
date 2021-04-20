8169f7c939cd97e15fc107fdff820ca9a58721a9
David <david.bechkam@hotmail.com>
2021-04-20 17:26:06 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+  
+const colors = JSON.parse(dmonmadColors);
+
+console.log(colors);
