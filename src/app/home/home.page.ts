502ac1fa358da9a200c9c2b8162d5f09851addd9
FUNDACION\David.monserrat <david.monserrat@e-externas.fundae.es>
2023-06-08 18:54:50 +0200

    no message

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,25 +14,21 @@ export class HomePage {
   }
 
   async onScroll(e: any) {
+    console.log(e);
     const scrollElement = await e.target.getScrollElement();
-    console.log({scrollElement});
 
     // minus clientHeight because trigger is scrollTop
     // otherwise you hit the bottom of the page before 
     // the top screen can get to 80% total document height
     const scrollHeight = scrollElement.scrollHeight - scrollElement.clientHeight;
-    console.log({scrollHeight});
 
     const currentScrollDepth = scrollElement.scrollTop;
-    console.log({currentScrollDepth});
 
-    const targetPercent = 80;
-
-    let triggerDepth = ((scrollHeight / 100) * targetPercent);
-    console.log({triggerDepth});
+    let scrollPercentage = ((currentScrollDepth / scrollHeight) * 100);
+    console.log({triggerDepth: scrollPercentage});
 
 
-    this.scrollPercentage = currentScrollDepth + "%";
+    this.scrollPercentage = scrollPercentage + '%';
 
   }
 
